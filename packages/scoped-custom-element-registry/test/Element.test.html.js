bb2bcc613fa6ed4f107ec166dcd856309e748409
Jesse van Assen <jesse.v.assen@gmail.com>
2023-08-21 14:38:15 +0200

    Add attributeNode tests for scoped-custom-element-registry

--- packages/scoped-custom-element-registry/test/Element.test.html.js
+++ packages/scoped-custom-element-registry/test/Element.test.html.js
@@ -151,5 +151,140 @@ describe('Element', () => {
 
       expect($el.hasAttribute('foo')).to.be.true;
     });
+
+    it('should call setAttributeNode', () => {
+      const {tagName, CustomElementClass} = getObservedAttributesTestElement([
+        'foo',
+      ]);
+      customElements.define(tagName, CustomElementClass);
+      const $el = document.createElement(tagName);
+
+      const attr = document.createAttribute('foo');
+      attr.value = 'bar';
+      $el.setAttributeNode(attr);
+
+      expect($el.getAttribute('foo')).to.equal('bar');
+
+      /* Updating a node's value after it has been added to an element should
+       * update the attribute's value. */
+      attr.value = 'baz';
+      expect($el.getAttribute('foo')).to.equal('baz');
+    });
+
+    it('setAttributeNode should trigger attributeChangedCallback', () => {
+      const {tagName, CustomElementClass} = getObservedAttributesTestElement([
+        'foo',
+      ]);
+      customElements.define(tagName, CustomElementClass);
+      const $el = document.createElement(tagName);
+
+      const attr = document.createAttribute('foo');
+      attr.value = 'bar';
+      $el.setAttributeNode(attr);
+      attr.value = 'baz';
+
+      expect($el.changedAttributes).to.deep.equal([
+        {
+          name: 'foo',
+          oldValue: null,
+          newValue: 'bar',
+        },
+        {
+          name: 'foo',
+          oldValue: 'bar',
+          newValue: 'baz',
+        },
+      ]);
+    });
+
+    it('changing an existing attribute should trigger attributeChangedCallback', () => {
+      const {tagName, CustomElementClass} = getObservedAttributesTestElement([
+        'foo',
+        'bar',
+      ]);
+      customElements.define(tagName, CustomElementClass);
+      const $el = getHTML(`<${tagName} foo="value 1"></${tagName}>`);
+      $el.setAttribute('bar', 'value 1');
+
+      $el.getAttributeNode('foo').value = 'value 2';
+      $el.getAttributeNode('bar').value = 'value 2';
+
+      expect($el.changedAttributes.slice(2)).to.deep.equal([
+        {
+          name: 'foo',
+          oldValue: 'value 1',
+          newValue: 'value 2',
+        },
+        {
+          name: 'bar',
+          oldValue: 'value 1',
+          newValue: 'value 2',
+        },
+      ]);
+    });
+
+    it('iterating over attributes and changing them should trigger attributeChangedCallback', () => {
+      const {tagName, CustomElementClass} = getObservedAttributesTestElement([
+        'foo',
+        'bar',
+      ]);
+      customElements.define(tagName, CustomElementClass);
+      const $el = getHTML(`<${tagName} foo="value 1"></${tagName}>`);
+      $el.setAttribute('bar', 'value 1');
+
+      for (const attr of $el.attributes) {
+        attr.value = 'value 2';
+      }
+
+      expect($el.changedAttributes.slice(2)).to.deep.equal([
+        {
+          name: 'foo',
+          oldValue: 'value 1',
+          newValue: 'value 2',
+        },
+        {
+          name: 'bar',
+          oldValue: 'value 1',
+          newValue: 'value 2',
+        },
+      ]);
+    });
+
+    it('should call removeAttributeNode', () => {
+      const {tagName, CustomElementClass} = getObservedAttributesTestElement([
+        'foo',
+      ]);
+      customElements.define(tagName, CustomElementClass);
+      const $el = document.createElement(tagName);
+
+      const attr = document.createAttribute('foo');
+      attr.value = 'bar';
+      $el.setAttributeNode(attr);
+
+      $el.removeAttributeNode(attr);
+      expect($el.hasAttribute('foo')).to.be.false;
+    });
+
+    it('removeAttributeNode should trigger attributeChangedCallback', () => {
+      const {tagName, CustomElementClass} = getObservedAttributesTestElement([
+        'foo',
+      ]);
+      customElements.define(tagName, CustomElementClass);
+      const $el = getHTML(`<${tagName} foo="value"></${tagName}>`);
+      $el.removeAttributeNode($el.getAttributeNode('foo'));
+
+      expect($el.changedAttributes).to.deep.equal([
+        {
+          name: 'foo',
+          oldValue: null,
+          newValue: 'value',
+        },
+        {
+          name: 'foo',
+          oldValue: 'value',
+          newValue: null,
+        },
+      ]);
+    });
   });
 });
